5c4f58e6fce482f495b1e80e84769702955cdd20
s22017 <itc-s22017@std.it-college.ac.jp>
2024-07-30 10:50:22 +0900

    deleted chat.tsx

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const JoinContainer = styled.div`
   display: flex;
